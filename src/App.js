5f687913d3d760922dc17f400cf0e6e640877798
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2023-07-01 18:50:22 +0100

    game state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Sidebar from './components/Sidebar';
 import { Grid, Paper } from '@mui/material';
 import TicTacToe from './games/TicTacToe';
+import RockPaperScissors from './games/RockPaperScissors.js';
+import Sudoku from './games/Sudoku.js';
 
 function App() {
+  const [game, setGame] = useState('Tic Tac Toe');
   const [darkMode, setDarkMode] = useState(false);
   const darkTheme = createTheme({
     palette: {
@@ -14,12 +17,24 @@ function App() {
     },
   });
 
+  const GameComponent = () => {
+    const gameComponents = {
+      'Tic Tac Toe': TicTacToe,
+      'Rock Paper Scissors': RockPaperScissors,
+      'Sudoku': Sudoku,
+    };
+  
+    const SelectedGame = gameComponents[game] || null;
+  
+    return SelectedGame ? <SelectedGame /> : null;
+  };
+  
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <div>
         <Navbar check={darkMode} change={() => setDarkMode(!darkMode)} />
-        <Sidebar />
+        <Sidebar setGame={setGame} />
         <Grid
           container
           direction="row"
@@ -29,7 +44,7 @@ function App() {
         >
           <Grid item padding={2}>
             <Paper sx={{ padding: 2 }}>
-              <TicTacToe />
+              <GameComponent />
             </Paper>
           </Grid>
         </Grid>
@@ -39,4 +54,3 @@ function App() {
 }
 
 export default App;
-
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,49 +1,17 @@
-// import React from 'react';
-// import { Drawer, List, ListItem, ListItemText } from '@mui/material';
-
-// const Sidebar = () => {
-//   return (
-//     <Drawer variant="permanent" PaperProps={{ style: { width: 200, marginTop: 69 } }}>
-//       <List>
-//         <ListItem button>
-//           <ListItemText primary="Home" />
-//         </ListItem>
-//         <ListItem button>
-//           <ListItemText primary="About" />
-//         </ListItem>
-//         <ListItem button>
-//           <ListItemText primary="Services" />
-//         </ListItem>
-//         <ListItem button>
-//           <ListItemText primary="Contact" />
-//         </ListItem>
-//       </List>
-//     </Drawer>
-//   );
-// };
-
-// export default Sidebar;
 import * as React from 'react';
 import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import VideogameAssetSharpIcon from '@mui/icons-material/VideogameAssetSharp';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 
 const drawerWidth = 240;
 
@@ -56,7 +24,7 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   justifyContent: 'flex-end',
 }));
 
-export default function Sidebar() {
+export default function Sidebar({ setGame }) {
   const theme = useTheme();
   const [open, setOpen] = React.useState(true);
   const isMobileView = window.innerWidth <= 600;
@@ -69,16 +37,21 @@ export default function Sidebar() {
   const handleDrawerClose = () => {
     setOpen(false);
   };
+  const handleGameSelect = (game) => {
+    setGame(game);
+    handleDrawerClose();
+  };
 
   return (
-    <Box sx={{ display: 'flex' }} 
-    >
-      <Toolbar sx={{ paddingTop: 1 }} style={{ position: 'absolute', top: topPosition, left: 0, zIndex: 2 }}>
+    <Box sx={{ display: 'flex' }}>
+      <Toolbar
+        sx={{ paddingTop: 1 }}
+        style={{ position: 'absolute', top: topPosition, left: 0, zIndex: 2 }}
+      >
         <IconButton
           color="inherit"
           aria-label="open drawer"
           onClick={handleDrawerOpen}
-          edge="center"
           size="large"
           // sx={{ ...(open && { display: 'none' }) }}
         >
@@ -111,16 +84,15 @@ export default function Sidebar() {
         </DrawerHeader>
         <Divider />
         <List>
-          {['Tic Tac Toe', 'Rock Paper Scissors', 'Sudoku'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                {/* <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon> */}
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+          {['Tic Tac Toe', 'Rock Paper Scissors', 'Sudoku'].map(
+            (text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton onClick={() => handleGameSelect(text)}>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            )
+          )}
         </List>
       </Drawer>
     </Box>
